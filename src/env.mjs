82f71e5a5e62ccbc55bab3ff87f901629427954d
Rodrigo Iloro <rodrigo.iloro@gmail.com>
2023-09-05 20:30:34 -0300

    chore: fixing type errors.

--- src/env.mjs
+++ src/env.mjs
@@ -4,7 +4,7 @@ import { z } from "zod";
 export const env = createEnv({
   server: {
     POSTGRES_DATABASE: z.string().min(1),
-    POSTGRES_HOST: z.string().url(),
+    POSTGRES_HOST: z.string().min(1),
     POSTGRES_USER: z.string().min(1),
     POSTGRES_PASSWORD: z.string().min(1),
     POSTGRES_PRISMA_URL: z.string().url(),
@@ -15,6 +15,8 @@ export const env = createEnv({
     INNGEST_SIGNING_KEY: z.string().min(1),
 
     RESEND_API_KEY: z.string().min(1),
+
+    EMAIL_FROM_NAME: z.string().min(1),
     EMAIL_FROM: z.string().email(),
     EMAIL_TO: z.string().email(),
     EMAIL_SUBJECT: z.string().min(1),
