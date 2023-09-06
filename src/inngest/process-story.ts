e32cfac2179cb582b65f8b181d856c9b78967a2e
Rodrigo Iloro <rodrigo.iloro@gmail.com>
2023-09-06 14:33:53 -0300

    refactor:transforming process story into a multi-step function.

--- src/inngest/process-story.ts
+++ src/inngest/process-story.ts
@@ -1,5 +1,6 @@
 import { inngest } from "@/inngest/client";
-import { sql } from "@vercel/postgres";
+import { db, Posts } from "@/data/client";
+import { sql } from "drizzle-orm";
 import { z } from "zod";
 
 const storySchema = z.object({
@@ -14,18 +15,23 @@ export const processStory = inngest.createFunction(
   { event: "process-story" },
   async ({ event, step }) => {
     const storyId = event.data.storyId;
-    const request = await fetch(
-      `https://hacker-news.firebaseio.com/v0/item/${storyId}.json`
-    );
-    const story = await storySchema.parseAsync(await request.json());
-    const { id, title, url, time } = story;
 
-    const storyURL = url ?? `https://news.ycombinator.com/item?id=${id}`;
+    const story = await step.run("Fetch Story from Hacker News", async () => {
+      const request = await fetch(
+        `https://hacker-news.firebaseio.com/v0/item/${storyId}.json`
+      );
+      return await storySchema.parseAsync(await request.json());
+    });
 
-    const result = await sql`
-      INSERT INTO posts (postId, title, url, publishedAt)
-      VALUES (${id}, ${title}, ${storyURL}, to_timestamp(${time}))
-    `;
+    const result = await step.run("Insert Story into Database", async () => {
+      const { id, title, url, time } = story;
+      return await db.insert(Posts).values({
+        postId: id,
+        title,
+        url: url ?? `https://news.ycombinator.com/item?id=${id}`,
+        publishedAt: sql`to_timestamp(${time})`,
+      });
+    });
 
     return { event, body: { result } };
   }
